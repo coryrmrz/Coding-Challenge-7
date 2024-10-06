023d21aafb8352799ecec043ce7453eecb6b14ad
coryrmrz <coryramirez2018@gmail.com>
2024-10-06 01:58:12 -0400

    Calculate total salary for all departments in the company

--- department_salary.js
+++ department_salary.js
@@ -54,19 +54,31 @@ const company = {
 
 //Task 2: Create a Recursive Function to Calculate Total Salary for a Department
 function calculateDepartmentSalary(department) { //creating function to calculate salsries in different departments
-    let totalSalary = 0; //intializing total salary variable
+    let totalDepartmentSalary = 0; //intializing total department salary variable
 
     department.employees.forEach(employee => {
-        totalSalary += employee.salary; //finding total salary of each employee and adding them for total salary
+        totalDepartmentSalary += employee.salary; //finding total salary of each employee and adding them for total salary
 
         if (employee.subordinates && employee.subordinates.length > 0) {
-            totalSalary += calculateDepartmentSalary({employees: employee.subordinates}); //if employees have subordinates, then salary will also be added
+            totalDepartmentSalary += calculateDepartmentSalary({employees: employee.subordinates}); //if employees have subordinates, then salary will also be added
         }
     });
-    return totalSalary; //return total salary
+    return totalDepartmentSalary; //return total company salary
 }
 const engineeringSalary = calculateDepartmentSalary(company.departments[0]); //use function on engineering department
 console.log(`Total salary for ${company.departments[0].departmentName} is ${engineeringSalary}`); //Ouput engineering department total salary
 
 const salesSalary = calculateDepartmentSalary(company.departments[1]); //use function on sales department
 console.log(`Total salary for ${company.departments[1].departmentName} is ${salesSalary}`); //output sales department total salary
+
+//Task 3: Create a Function to Calculate the Total Salary for All Departments
+function calculateCompanySalary(company) { //creating function to calculate salary of the whole company
+    let totalCompanySalary = 0; //initializing total company salary variable
+    
+    company.departments.forEach(department => {
+        totalCompanySalary += calculateDepartmentSalary(department); //finding total salary of each department and adding them for total salary
+    });
+    return totalCompanySalary //return total company salary
+}
+const totalSalary = calculateCompanySalary(company); //use function to get company salary
+console.log(`The company's total salary is ${totalSalary}`) //output total company salary
