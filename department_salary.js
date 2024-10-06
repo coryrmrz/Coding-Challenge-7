2e8e7f0d80d168963e9c319bdd816addbd6b8392
coryrmrz <coryramirez2018@gmail.com>
2024-10-06 01:49:34 -0400

    Create recursive function to calculate department salary

--- department_salary.js
+++ department_salary.js
@@ -33,7 +33,7 @@ const company = {
             employees: [ //array of employees in Sales department
                 {
                     name: 'Eve',
-                    salary: 85000
+                    salary: 85000,
                     subordinates: [ //employee Eve with subordinate Frank
                         {
                             name: 'Frank',
@@ -53,16 +53,20 @@ const company = {
 };
 
 //Task 2: Create a Recursive Function to Calculate Total Salary for a Department
-function calculateDepartmentSalary(department) {
-    let totalSalary = 0;
+function calculateDepartmentSalary(department) { //creating function to calculate salsries in different departments
+    let totalSalary = 0; //intializing total salary variable
 
     department.employees.forEach(employee => {
-        totalSalary += employee.salary;
+        totalSalary += employee.salary; //finding total salary of each employee and adding them for total salary
 
         if (employee.subordinates && employee.subordinates.length > 0) {
-            totalSalary += calculateDepartmentSalary({employees: employee.subordinates});
+            totalSalary += calculateDepartmentSalary({employees: employee.subordinates}); //if employees have subordinates, then salary will also be added
         }
     });
-    return totalSalary;
+    return totalSalary; //return total salary
 }
-console.log(`Total salary for ${departmentName} is ${totalSalary}}`)
+const engineeringSalary = calculateDepartmentSalary(company.departments[0]); //use function on engineering department
+console.log(`Total salary for ${company.departments[0].departmentName} is ${engineeringSalary}`); //Ouput engineering department total salary
+
+const salesSalary = calculateDepartmentSalary(company.departments[1]); //use function on sales department
+console.log(`Total salary for ${company.departments[1].departmentName} is ${salesSalary}`); //output sales department total salary
